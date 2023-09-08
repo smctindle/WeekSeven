14679ea9ff7cac61d86e20c6c0c46cebc5219a0d
smctindle <smctindle@gmail.com>
2023-09-08 19:26:39 -0400

    Adding week 9 assignment

--- src/main/java/projects/dao/DbConnection.java
+++ src/main/java/projects/dao/DbConnection.java
@@ -7,24 +7,22 @@ import java.sql.SQLException;
 import projects.exception.DbException;
 
 public class DbConnection {
-
-private static String Host = "localhost"; 
-private static String Password = "projetcs";
-private static int Port = 3306;
 private static String Schema = "projects";
-private static String User = "projects"; 
+private static String User = "projects";
+private static String Password = "projects";
+private static String Host = "localhost"; 
+private static int Port = 3306;
+ 
 
 public static Connection getConnection() {
 	String url = String.format("jdbc:mysql://%s:%d/%s?user=%s&password=%s&useSSL=false",
 					Host, Port, Schema, User, Password);
 	
-	System.out.println("Connecting with url=" + url);
-	
+	System.out.println("Connecting with url=" + url);	
 	try {
 		Connection conn = DriverManager.getConnection(url);
 		System.out.println("Connection successfully!!");
-		return conn;
-		
+		return conn;	
 	} catch (SQLException e) {
 		System.out.println("Error with connection");
 		throw new DbException(e); 
--- src/main/java/projects/exception/DbException.java
+++ src/main/java/projects/exception/DbException.java
@@ -3,36 +3,25 @@
  */
 package projects.exception;
 
-/**
- * 
- */
+
 @SuppressWarnings("serial")
 public class DbException extends RuntimeException {
 
-	/**
-	 * 
-	 */
+	
 	public DbException() {
 	}
 
-	/**
-	 * @param message
-	 */
+	
 	public DbException(String message) {
 		super(message);
 	}
 
-	/**
-	 * @param cause
-	 */
+
 	public DbException(Throwable cause) {
 		super(cause);
 	}
 
-	/**
-	 * @param message
-	 * @param cause
-	 */
+	
 	public DbException(String message, Throwable cause) {
 		super(message, cause);
 	}
